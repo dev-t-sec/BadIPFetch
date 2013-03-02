b3f124ad1e8737c69c60fe0c3bde59703541ee54
Markus Schmall <schmall.markus@gmail.com>
2013-03-02 16:51:08 +0100

    added start script for cron job alike calls

---
+++ start.sh
@@ -0,0 +1,2 @@
+java -cp ./badipfetch.jar:./libs/httpclient-cache-4.2.2.jar:./libs/httpclient-4.2.2.jar:./libs/httpcore-4.2.3.jar:./libs/jedis-2.1.0.jar:./libs/commons-logging-1.1.1.jar org.metams.badipfetch.OneShot $1 $2
+
