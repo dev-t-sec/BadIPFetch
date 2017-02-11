9898fb539fc78507dabc2e1ec96b7b4c65728a0e
schmalle <schmall.markus@gmail.com>
2017-02-11 22:52:08 +0100

    updated libraries

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -cp ./badipfetch.jar:./libs/httpclient-cache-4.3.6.jar:./libs/httpclient-4.3.6.jar:./libs/httpcore-4.3.3.jar:./libs/jedis-2.1.0.jar:./libs/commons-logging-1.1.3.jar org.metams.badipfetch.OneShot $1 $2 $3
+java -cp ./badipfetch.jar:./libs/httpclient-cache-4.5.3.jar:./libs/httpclient-4.5.3.jar:./libs/httpcore-4.4.6.jar:./libs/jedis-2.9.0.jar:./libs/commons-logging-1.2.jar org.metams.badipfetch.OneShot $1 $2 $3
 
